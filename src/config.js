64011004ed31b13141f654aa9dc126b8fdac2338
Iyowun <35067650+Iyowun@users.noreply.github.com>
2023-02-21 21:31:52 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 exports.parentSitemapSize = 2000; // add 50 to robots.txt
 exports.childSitemapSize = 12500; // add 50k to each parent sitemap
-exports.pageUrlSize = 1000; // add 50k to each child sitemap
+exports.pageUrlSize = 5000; // add 50k to each child sitemap
 
 // Expenses configs
 
