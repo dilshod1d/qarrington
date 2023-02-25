e905a5d691493eb1377bd37e56f7b12f977a8c6f
Iyowun <35067650+Iyowun@users.noreply.github.com>
2023-02-25 06:26:08 +0300

    expense-limits-

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ exports.pageUrlSize = 2500; // add 50k to each child sitemap
 
 // Expenses configs
 
-exports.expenses_parentSitemapSize = 1000; // add 10 to robots.txt
+exports.expenses_parentSitemapSize = 40; // add 10 to robots.txt
 exports.expenses_childSitemapSize = 1000; // add 50k to each parent sitemap
 exports.expenses_pageUrlSize = 1000; // add 50k to each child sitemap
