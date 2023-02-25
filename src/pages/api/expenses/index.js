a1bdde9369769e1e5b6963075fa83beae96d7445
goldenfay <s.faycal@ouedkniss.com>
2023-02-25 11:18:21 +0100

    Optimize expenses fetch for sitemaps

--- src/pages/api/expenses/index.js
+++ src/pages/api/expenses/index.js
@@ -4,6 +4,7 @@ import Expense from '../../../../models/expense/Expense';
 async function handler(req, res) {
   const { method } = req;
   const { expenseUrl } = req.query;
+  const { query } = req.query;
 
   await dbConnect();
 
@@ -18,9 +19,22 @@ async function handler(req, res) {
         res.status(500).json(err);
       }
     } else {
+      
       try {
-        const readItems = await Expense.find();
-        res.status(200).json(readItems);
+        if (query === 'expense-count') {
+          const count = await Destination.count();
+          return res.status(200).json({ count });
+        }  else if (query === 'expense-sitemap') {
+          const { offset } = req.query;
+          const expenses =  await Expense.find()
+          .select({ expenseUrl: 1 })
+          .limit(1)
+          .skip(offset);
+          return res.status(200).json({ expenses });
+        } else {
+          const readItems = await Expense.find();
+          return res.status(200).json(readItems);
+        }
       } catch (err) {
         res.status(500).json(err);
       }
--- src/pages/sitemap/expense/[child]/[subchild].js
+++ src/pages/sitemap/expense/[child]/[subchild].js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { getServerSideSitemap } from 'next-sitemap';
 import Expense from '../../../../../models/expense/Expense';
 import Location from '../../../../../models/location/Location';
@@ -7,21 +8,41 @@ import {
 } from '../../../../config';
 
 const siteUrl = process.env.NEXT_PUBLIC_APP_URL;
+const regex =
+  /((?:[\0-\x08\x0B\f\x0E-\x1F\uFFFD\uFFFE\uFFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF]))/g;
 
+const extract_attr = (obj, ...args) => {
+  let res = {};
+  if (Array.isArray(args)) {
+    args.forEach((arg) => (res[arg] = obj?.data[arg]));
+  }
+
+  return res;
+};
 export const getServerSideProps = async (ctx) => {
   const { child, subchild } = ctx.params;
   let fields = [];
   if (child && subchild) {
     if (/^[0-9]+$/g.test(child) && /^[0-9]+.xml$/g.test(subchild)) {
+      // Extract route params
+      let pageParam = Number(child);
+      let offsetParam = Number(subchild.slice(0, subchild.indexOf('.xml')));
+
+      try {
+      } catch (err) {
+        console.error('An Error Occured : ', err);
+        return {
+          props: {
+            serverErrors: JSON.stringify(err, Object.getOwnPropertyNames(err))
+          }
+        };
+      }
       let expCount = await Expense.count();
       let locCount = await Location.count();
       const totalRoutes = expCount * locCount;
       const nbrLocPages = Math.ceil(locCount / expenses_pageUrlSize);
 
-      // Extract route params
-      let pageParam = Number(child);
-      let offsetParam = Number(subchild.slice(0, subchild.indexOf('.xml')));
-
+      // Define offset & page size
       // let totalOffset =
       //   offsetParam * expenses_pageUrlSize +
       //   pageParam * expenses_childSitemapSize * expenses_pageUrlSize;
@@ -30,32 +51,37 @@ export const getServerSideProps = async (ctx) => {
         pageParam * expenses_childSitemapSize * locCount;
 
       // let pageOffset = Math.floor(totalOffset / nbrLocPages);
-      let pageOffset = Math.floor(pageParam);
+      let pageOffset = Math.floor(
+        (expCount / expenses_childSitemapSize) * pageParam + offsetParam
+      );
 
       // Get the expenses
-      const expenses = await Expense.find()
-        .select({ expenseUrl: 1 })
-        .limit(1)
-        .skip(pageOffset);
+      const {
+        data: { expenses }
+      } = await axios.get(
+        `${process.env.NEXT_PUBLIC_APP_URL}api/expenses?query=expense-sitemap&offset=${pageOffset}`,
+        { headers: { 'Content-Type': 'application/json' } }
+      );
       // Get the locations
       const locations = await Location.find()
         .select({ locationUrl: 1 })
         .limit(expenses_pageUrlSize)
         .skip(offsetParam);
       expenses.forEach((expense) => {
+        const expenseUrl = expense.expenseUrl
+          .replace(/\s+/g, '-')
+          .replace(/&(?!#?[a-z0-9]+;)/, '&amp;')
+          .replace(regex, '')
+          .toLowerCase();
         // Add the expense alone
         fields.push({
-          loc: `${siteUrl}expense/${expense.expenseUrl
-            .replace(/\s+/g, '-')
-            .toLowerCase()}`,
+          loc: `${siteUrl}expense/${expenseUrl}`,
           lastmod: new Date().toISOString()
         });
         // Add all the locations to that specific expense
         locations.forEach((location) => {
           fields.push({
-            loc: `${siteUrl}expense/${expense.expenseUrl
-              .replace(/\s+/g, '-')
-              .toLowerCase()}/${location.locationUrl
+            loc: `${siteUrl}expense/${expenseUrl}/${location.locationUrl
               .replace(/\s+/g, '-')
               .toLowerCase()
               .replace(/&(?!#?[a-z0-9]+;)/, '&amp;')}`,
@@ -78,4 +104,7 @@ export const getServerSideProps = async (ctx) => {
   return getServerSideSitemap(ctx, fields);
 };
 
-export default function Sitemap() {}
+export default function Sitemap(props) {
+  console.log(props);
+  return <></>;
+}
