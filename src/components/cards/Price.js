3c9c1dfca91067175b4d80fe0826150ea16573e0
Iyowun <35067650+Iyowun@users.noreply.github.com>
2023-03-09 22:53:00 +0300

    Update Price.js

--- src/components/cards/Price.js
+++ src/components/cards/Price.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { pink } from '@mui/material/colors';
-import { Box, Card, Grid, Tooltip, Typography } from '@mui/material'
+import { green } from '@mui/material/colors';
+import { Box, Card, Divider, Grid, Tooltip, Typography } from '@mui/material'
 import FooterMenu from '../menus/FooterMenu';
 import useSWR from 'swr';
 
@@ -20,61 +20,104 @@ const Component = () => {
 
           <Card style={{ padding: '40px', marginBottom: '10px' }}>
 
-            <Box width="100%" height="100%">
-
-              {companies && companies.slice(0, 1).map(({ _id, companyTicker, companyKpi, companyListing }) => (
-                <>
-                  {companyKpi && companyKpi.slice(0, 1).map(({ _id, companyCurrency, companyCapitalization, companyVolume, companyPrice, companyPriceVariant, companyPercentChange, companyPointChange, companyActiveCustomers, companyIsRecordedAt }) => (
-                    <Box key={_id} textAlign="center" marginBottom="10px">
-                      <Box>
-                        <Typography fontSize="32px" fontWeight={700} component="span" color="black">
-                          {companyPrice}
-                        </Typography>
-                      </Box>
-
-                      <Box>
-                        <Typography fontSize="16px" fontWeight={600} component="span" color={companyPriceVariant}>
-                          {`+${companyPointChange}`}
-                        </Typography>
-                        <Typography variant="body2" component="span" color="secondary" marginX={0.5}>
-                          \
-                        </Typography>
-                        <Typography fontSize="14px" fontWeight={600} component="span" color={companyPriceVariant}>
-                          {`+${companyPercentChange}`}%
-                        </Typography>
-                      </Box>
-
-                      <Box>
-                        <Typography style={Data2Item} component="span" color="black">
-                          {companyVolume}
-                        </Typography>
-                        <Typography style={Data2Helper} component="span" color="secondary">
-                          Vol
-                        </Typography>
-                        <Typography style={Data2Item} component="span" color="black">
-                          {`${companyCapitalization}`}
-                        </Typography>
-                        <Typography style={Data2Helper} component="span" color="secondary">
-                          Cap
-                        </Typography>
-                      </Box>
+            {companies && companies.slice(0, 1).map(({ _id, companyTicker, companyKpi, companyListing }) => (
+              <>
+                {companyKpi && companyKpi.slice(0, 1).map(({ _id, companyCurrency, companyCapitalization, companyVolume, companyPrice, companyPriceVariant, companyPercentChange, companyPointChange, companyActiveCustomers, companyIsRecordedAt }) => (
+                  <Box key={_id} textAlign="center" marginBottom="10px">
+                    <Box>
+                      <Typography fontSize="32px" fontWeight={700} component="span" color="black">
+                        ${companyPrice}
+                      </Typography>
                     </Box>
-                  ))}
-                </>
-              ))}
-            </Box>
 
-            <Grid item xs={12} mb={2}>
-              <Grid container spacing={2}>
+                    <Box>
+                      <Typography fontSize="16px" fontWeight={600} component="span" color={companyPriceVariant}>
+                        {`+${companyPointChange}`}
+                      </Typography>
+                      <Typography variant="body2" component="span" color="secondary" marginX={0.5}>
+                        \
+                      </Typography>
+                      <Typography fontSize="14px" fontWeight={600} component="span" color={companyPriceVariant}>
+                        {`+${companyPercentChange}`}%
+                      </Typography>
+                    </Box>
+
+                    <Box>
+                      <Typography style={Data2Item} component="span" color="black">
+                        {companyVolume}
+                      </Typography>
+                      <Typography style={Data2Helper} component="span" color="secondary">
+                        Vol
+                      </Typography>
+                      <Typography style={Data2Item} component="span" color="black">
+                        {`${companyCapitalization}`}
+                      </Typography>
+                      <Typography style={Data2Helper} component="span" color="secondary">
+                        Cap
+                      </Typography>
+                    </Box>
+                  </Box>
+                ))}
+              </>
+            ))}
+
+            <Grid item xs={12} mb={0}>
+              <Grid container spacing={1}>
                 {cubes && cubes.map(({ _id, date, price, variant }) => (
                   <Tooltip title={date} placement="top">
-                    <Grid key={_id} item xs={12} sm={6} md={6} lg={1}>
-                      <Box style={{ padding: '7px', border: '1px', borderRadius: '3px', backgroundColor: pink[variant] }}>
+                    <Grid key={_id} item xs={12} sm={6} md={6} lg={1} mb={-0.5}>
+                      <Box style={{ padding: '7px', border: '1px', borderRadius: '3px', backgroundColor: green[variant] }}>
                         <></>
                       </Box>
                     </Grid>
                   </Tooltip>
                 ))}
+              </Grid>
+            </Grid>
+
+            <Grid item xs={12} mt={1}>
+              <Grid container spacing={1} display="flex" justifyContent="center">
+                {/* <Grid item xs={12} sm={1.5} mt={-0.4}>
+                  <Typography textAlign="right" fontWeight="600" color="secondary" component="span" fontSize="11px">FALL</Typography>
+                </Grid> */}
+                <Tooltip title="low" placement="top">
+                  <Grid item xs={12} sm={6} md={6} lg={1}>
+                    <Box style={{ padding: '7px', border: '1px', borderRadius: '3px', backgroundColor: green[100] }}>
+                      <></>
+                    </Box>
+                  </Grid>
+                </Tooltip>
+                {/* <Tooltip title="low" placement="top">
+                  <Grid item xs={12} sm={6} md={6} lg={1}>
+                    <Box style={{ padding: '7px', border: '1px', borderRadius: '3px', backgroundColor: green[300] }}>
+                      <></>
+                    </Box>
+                  </Grid>
+                </Tooltip> */}
+                <Tooltip title="mid" placement="top">
+                  <Grid item xs={12} sm={6} md={6} lg={1}>
+                    <Box style={{ padding: '7px', border: '1px', borderRadius: '3px', backgroundColor: green[500] }}>
+                      <></>
+                    </Box>
+                  </Grid>
+                </Tooltip>
+                {/* <Tooltip title="low" placement="top">
+                  <Grid item xs={12} sm={6} md={6} lg={1}>
+                    <Box style={{ padding: '7px', border: '1px', borderRadius: '3px', backgroundColor: green[700] }}>
+                      <></>
+                    </Box>
+                  </Grid>
+                </Tooltip> */}
+                <Tooltip title="high" placement="top">
+                  <Grid item xs={12} sm={6} md={6} lg={1}>
+                    <Box style={{ padding: '7px', border: '1px', borderRadius: '3px', backgroundColor: green[900] }}>
+                      <></>
+                    </Box>
+                  </Grid>
+                </Tooltip>
+                {/* <Grid item xs={12} sm={2} mt={-0.4}>
+                  <Typography textAlign="left" fontWeight="600" color="secondary" component="span" fontSize="11px">RISE</Typography>
+                </Grid> */}
               </Grid>
             </Grid>
 
@@ -106,39 +149,243 @@ const Data2Helper = {
 
 const cubes = [
   {
-    _id: 1,
-    date: "one1",
+    date: "Fri, May 22, 2023, 7:45 PM",
     price: "20",
     variant: 300
   },
   {
-    _id: 2,
-    date: "one2",
+    date: "Fri, May 22, 2023, 7:45 PM",
     price: "20",
     variant: 600
   },
   {
-    _id: 3,
-    date: "one3",
+    date: "Fri, May 22, 2023, 7:45 PM",
     price: "20",
     variant: 200
   },
   {
-    _id: 4,
-    date: "one3",
+    date: "Fri, May 22, 2023, 7:45 PM",
     price: "20",
     variant: 100
   },
   {
-    _id: 5,
-    date: "one3",
+    date: "Fri, May 22, 2023, 7:45 PM",
     price: "20",
     variant: 800
   },
   {
-    _id: 6,
-    date: "one3",
+    date: "Fri, May 22, 2023, 7:45 PM",
     price: "20",
     variant: 500
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 300
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 600
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 200
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 100
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 800
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 500
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 300
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 600
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 200
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 100
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 800
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 500
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 100
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 800
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 500
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 300
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 600
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 200
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 100
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 800
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 100
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 800
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 500
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 300
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 600
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 200
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 100
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 800
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 100
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 800
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 500
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 300
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 600
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 200
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 100
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 800
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 100
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 800
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 500
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 300
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 600
+  },
+  {
+    date: "Fri, May 22, 2023, 7:45 PM",
+    price: "20",
+    variant: 200
   }
 ]
