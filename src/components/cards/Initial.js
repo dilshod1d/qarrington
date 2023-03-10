efbaa1632dd4a68695dba7963046baa319f86b49
Iyowun <35067650+Iyowun@users.noreply.github.com>
2023-03-10 22:54:52 +0300

    Update Initial.js

--- src/components/cards/Initial.js
+++ src/components/cards/Initial.js
@@ -1,66 +1,77 @@
+import React from 'react';
 import {
   Avatar,
-  AvatarGroup,
+  Badge,
   Box,
-  Breadcrumbs,
   Button,
   Card,
   Grid,
   styled,
+  Tooltip,
   Typography
 } from '@mui/material';
 import Link from 'next/link';
-import React from 'react';
 import FooterMenu from '../menus/FooterMenu';
+import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
 import useSWR from 'swr';
 
 const Component = () => {
 
   const fetcher = (...args) => fetch(...args).then(res => res.json());
   const { data: companies } = useSWR(`${process.env.NEXT_PUBLIC_APP_URL}/api/companies`, fetcher);
-  
+
   return (
     <div style={{ position: 'sticky', top: '100px' }}>
       {/* RightGrid Starts Here */}
 
       <Grid container spacing={1}>
         <Grid item xs={12}>
-          <Card style={{ padding: '40px' }}>
-            <Box style={{ display: 'flex', justifyContent: 'center' }}>
-              <AvatarGroup max={3}>
-                <Avatar style={{ width: 30, height: 30 }}
-                  alt="Qarringtons" src="/assets/media/accounts/male/065.webp" />
-                <Avatar style={{ width: 60, height: 60 }}
-                  alt="Qarringtons" src="/assets/media/accounts/female/090.webp" />
-                <Avatar style={{ width: 30, height: 30 }}
-                  alt="Qarringtons" src="/assets/media/accounts/male/076.webp" />
-              </AvatarGroup>
-            </Box>
-            <Box style={{ padding: '15px 0px 15px 0px', display: 'flex', justifyContent: 'center' }}>
-              <Typography
-                component="span"
-                variant="body"
-                fontSize="13px"
-                textAlign="center"
-                fontWeight={500}
-                color="secondary"
-              >
-                An <b>Initial Subscription Offering</b> or <b>ISO</b> allows you to utilize and monetize your subscriptions with technology companies.
-              </Typography>
-            </Box>
-            <Box>
-              <Link href="/challenges">
-                <Button
-                  size="medium"
-                  style={{ textTransform: 'uppercase', fontSize: '12px' }}
-                  variant="outlined"
-                  fullWidth={true}
-                >
-                  here's why
-                </Button>
-              </Link>
-            </Box>
-          </Card>
+          {companies && companies.slice(0, 1).map(({ _id, companyTicker, companyListing }) => (
+            <Card style={{ padding: '40px' }}>
+              <Box style={{ padding: '0px', textAlign: 'center' }}>
+                <Tooltip title="An Initial Subscription Offering or ISO allows you to utilize and monetize your subscriptions with SaaS companies." placement="top">
+                  <Box
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'center'
+                    }}
+                  >
+                    <Badge
+                      overlap="circular"
+                      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                      badgeContent={
+                        <InfoRoundedIcon fontSize="small" color="primary" />
+                      }
+                    >
+                      <Avatar
+                        style={{ width: 50, height: 50 }}
+                        alt="hello"
+                        src={companyListing.companyLogo}
+                      />
+                    </Badge>
+                  </Box>
+                </Tooltip>
+                <Typography component="div" variant="h1" fontWeight="700" color="black">
+                  000
+                </Typography>
+                <Typography mb={1} variant="body2" fontWeight="500" color="secondary">
+                  The <b>Initial Subscription Offering</b> or <b>ISO</b> of {companyTicker} starts in X days and will last for no more than 7 days.
+                </Typography>
+              </Box>
+              <Box>
+                <Link href={`/dashboard/${companyTicker}`}>
+                  <Button
+                    size="large"
+                    style={{ color: 'white', textTransform: 'uppercase', fontSize: '12px' }}
+                    variant="contained"
+                    fullWidth={true}
+                  >
+                    buy {companyTicker}
+                  </Button>
+                </Link>
+              </Box>
+            </Card>
+          ))}
 
           <FooterMenu />
 
