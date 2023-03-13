26096e41c949e6a51e3097480241ee325858a302
Iyowun <35067650+Iyowun@users.noreply.github.com>
2023-03-13 15:31:28 +0300

    still-err-

--- src/pages/api/accounts/index.js
+++ src/pages/api/accounts/index.js
@@ -1,11 +1,22 @@
 import dbConnect from '../../../lib/dbConnect';
 import Account from '../../../../models/account/Account';
+import handler, { initValidation, post, check } from "../../../middleware/handler"
+import { generateToken, getHeaderAuth } from "../../../lib/auth"
 
-async function handler(req, res) {
-  const { method } = req;
-  const { accountSlug } = req.query;
+const validator = initValidation(
+	[
+		check('accessKey').isLength({ min: 13 }).withMessage('Access Key is less than required length'),
+	]
+)
 
-  await dbConnect();
+// define my middleware here and use it only for POST requests
+export default handler
+	.use(validator)
+	.post(async (req, res) => {
+		await dbConnect();
+		const accountAccessKey = req.body.accessKey;
+		const accountSecretKey = generateToken(13)
+		// res.status(200).json(req.body);
 
 		if (accountAccessKey) {
 			try {
@@ -43,20 +54,15 @@ async function handler(req, res) {
 				res.status(400).json({ success: false, message:"Account Not Found" })
 			res.status(200).json({ success: true, data: { account } })
 
-  if (method === 'POST') {
-    try {
-      const createItem = await Account.create(req.body);
-      res.status(201).json(createItem);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  }
-}
-
-export default handler;
+			// const createItem = await Account.create(req.body);
+			// res.status(201).json(createItem);
+		} catch (err) {
+			res.status(500).json(err);
+		}
+	})
 
 export const config = {
-  api: {
-    responseLimit: false
-  }
+	api: {
+		responseLimit: false
+	}
 };
