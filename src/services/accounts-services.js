3b0d05c3cc3b9e66ae61adb192fa849f80176c97
whoknowsi <echosmania@gmail.com>
2023-03-21 20:20:35 -0300

    Added methods to accounts services

--- src/services/accounts-services.js
+++ src/services/accounts-services.js
@@ -2,10 +2,19 @@ import axios from 'axios'
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL
 
+export const getUserAccount = () => {
+  const url = `${apiUrl}/accounts`
+  return axios.get(url)
+}
+
 export const createAccount = ({ accessKey }) => {
-  console.log(apiUrl)
   const url = `${apiUrl}/accounts`
   return axios.post(url, {
     accessKey
   })
+}
+
+export const updateAccount = (data) => {
+  const url = `${apiUrl}/accounts/update`
+  return axios.put(url, data)
 }
