73f8e608c07f943dc5622e3862966a91f7aa4a9a
Iyowun <35067650+Iyowun@users.noreply.github.com>
2023-03-23 11:04:16 +0300

    fetch-err-

--- src/pages/api/cryptos/index.js
+++ src/pages/api/cryptos/index.js
@@ -1,25 +1,25 @@
 import dbConnect from '../../../lib/dbConnect';
-import Section from '../../../../models/section/Section';
+import Question from '../../../../models/crypto/Crypto';
 
 async function handler(req, res) {
   const { method } = req;
-  const { sectionUrl } = req.query;
+  const { cryptoUrl } = req.query;
 
   await dbConnect();
 
   // read items
 
   if (method === "GET") {
-    if (sectionUrl) {
+    if (cryptoUrl) {
       try {
-        const readItems = await Section.findOne({ sectionUrl: sectionUrl });
+        const readItems = await Question.findOne({ cryptoUrl: cryptoUrl });
         res.status(200).json(readItems);
       } catch (err) {
         res.status(500).json(err);
       }
     } else {
       try {
-        const readItems = await Section.find();
+        const readItems = await Question.find();
         res.status(200).json(readItems);
       } catch (err) {
         res.status(500).json(err);
@@ -31,7 +31,7 @@ async function handler(req, res) {
 
   if (method === 'POST') {
     try {
-      const createItem = await Section.create(req.body);
+      const createItem = await Question.create(req.body);
       res.status(201).json(createItem);
     } catch (err) {
       res.status(500).json(err);
